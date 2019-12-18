7fd78dbcacf2da2fc88e1e3dac0929ae6db40573
kapsner <lorenz.kapsner@web.de>
2019-12-18 22:02:50 +0100

    updated tests

--- tests/testthat/test_learner_classif_lightgbm.R
+++ tests/testthat/test_learner_classif_lightgbm.R
@@ -7,7 +7,7 @@ test_that(
     learner <- LearnerClassifLightGBM$new()
     expect_learner(learner)
     learner$param_set$values[["objective"]] <- "binary"
-    result <- run_autotest(learner, predict_types = "prob")
+    result <- run_autotest(learner)
     skip("Type error in score()")
     expect_true(result, info = result$error)
   }
